4052c9cef34b1e7bd545a9a54f1e2cd9f9b83d45
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-08 16:38:14 -0700

    my first source code on C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main prints the string in a put function
+ * this program prints 'Programming is like building a multilingual puzzle'
+ * Return: 0
+ * /
+ int main(void)
+ {
+ 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
