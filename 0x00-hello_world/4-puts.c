a6aeee8380f4d3c7460ec7d5a437a598891dea5e
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-08 16:42:47 -0700

    fixed a bug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * main prints the string in a put function
  * this program prints 'Programming is like building a multilingual puzzle'
  * Return: 0
- * /
+ */
  int main(void)
- {
+{
  	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
