4ac81d5394afd3dd8d79fb3c6f4bd26ffdb54e3b
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-08 16:57:19 -0700

    bugs fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main prints the string in a put function
+ * main - prints the string in a put function
  *
  * Description: using main function
  * this program prints 'Programming is like building a multilingual puzzle'
