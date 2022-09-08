0aab8bd8d9e8913fd17e0173d2d5fa7f1e409b1e
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-08 16:50:22 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 /**
  * main prints the string in a put function
+ *
+ * Description: using main function
  * this program prints 'Programming is like building a multilingual puzzle'
  * Return: 0
  */
