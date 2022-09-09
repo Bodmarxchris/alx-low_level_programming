7c38acb9db72ee0f8d7c71aeabd17e41f0dadd34
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-08 17:07:33 -0700

    print function usage

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ /**
+ * main - prints the string in the function
+ *
+ * Description: using the main function
+ * this program will print 'with proper grammar, but the outcome is a piece art, * '
+ * Return; 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0);
+}
