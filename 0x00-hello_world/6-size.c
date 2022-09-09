73e0ac0753f2aea32bf5e14ac5947f5f60a483fc
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-08 18:23:54 -0700

    C sucks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
- * 
+ * Description: using the function main 
  *
- * Return: always 0
+ * Return: 0
 */
-
 int main(void)
-
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
