baa1cdeeaaa893f104b7e6508cf147062457c918
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-09 15:56:51 -0700

    aphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Description: using the main function
+ *
+ * Return: alway returns 0
+ *
+ *
+ *
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
