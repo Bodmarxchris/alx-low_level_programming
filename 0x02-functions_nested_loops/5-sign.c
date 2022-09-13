26af2936806bcf3fa5dfe7edd84a4d2783b16da7
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-13 10:03:59 -0700

    pushed a fied bug

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - function to check for a sign of a number
- * @: is the int that will be used for the argument of the function
+ * @n: is the int that will be used for the argument of the function
  * Return: 0
  */
 int print_sign(int n)
