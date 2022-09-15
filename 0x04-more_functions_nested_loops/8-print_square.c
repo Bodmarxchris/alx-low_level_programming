737815925e56046ff5d8dc21332bb2c25c73b78b
omoniwa olabode chris <omoniwachris@gmail.com>
2022-09-15 09:24:34 -0700

    prnting squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * prnt_square - prints square using '#' character of a given size
+ * print_square - prints square using '#' character of a given size
  * @size: size of square
  *
  * Return: void
  */
+
 void print_square(int size)
 {
-	int i =0, c;
+	int i = 0, c;
 
 	if (size > 0)
 	{
 		while (i < size)
 		{
-			c =0;
+			c = 0;
 			while (c < size)
 			{
 				_putchar(35);
 				c++;
-			)
+			}
 			_putchar('\n');
 			i++;
 		}
