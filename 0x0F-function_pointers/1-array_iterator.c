5cfc40100043a6c177c6f5f1c5a22a015a436a2a
omoniwa olabode chris <omoniwachris@gmail.com>
2022-10-12 13:49:50 -0700

    fixed a bug

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * array_iterator - executes a function given as a parameter
@@ -10,7 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array && size > 0 && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (array == NULL || action == NULL)
+		exit(98);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * int_index - searches for an integer
@@ -12,9 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp && size > 0)
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
 	return (-1);
 }
