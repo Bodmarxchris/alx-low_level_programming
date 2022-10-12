dee39f8042a04384b5095dca72222ea99dd1723f
omoniwa olabode chris <omoniwachris@gmail.com>
2022-10-12 10:40:47 -0700

    bug fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef /* FUNCTION_POINTERS_H */
-#define /* FUNCTION_POINTERS_H */
+#ifndef FUNCTION_POINTER_H 
+#define FUNCTION_POINTER_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -7,4 +7,4 @@ int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 int _putchar(char c);
 
-#endif /* FUNCTION_POINTERS_H */
+#endif
